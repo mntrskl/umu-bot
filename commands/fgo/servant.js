c6741d4cd0e54d562f276094af6df6f03fbf0b5b
Diego <diego.89@gmail.com>
2018-04-23 23:37:47 -0300

    trying scraping method

--- commands/fgo/servant.js
+++ commands/fgo/servant.js
@@ -7,48 +7,7 @@ exports.run = async (client, message, args, level) => {
   if (!args || args.length < 1)
     return message.reply("Must provide a servant to search. Umu.");
 
-  snek
-    .get("https://fate-go.cirnopedia.org/servant_all.php#nav")
-    .then(r => r.body)
-    .then(r => {
-      var $ = cheerio.load(r);
-      const rows = $("tr.US");
-      const results = [];
-      const embed = new RichEmbed()
-        .setColor(0xff0000)
-        .setTitle(`Servant ID's Found`);
-
-      rows.each(function (i, element) {
-        const row = $(element);
-        const children = row.children("td")[3];
-        if (
-          $(children)
-            .attr("sorttable_customkey")
-            .toLowerCase()
-            .includes(args.map(arg => arg.toLowerCase()).join(" "))
-        ) {
-          //   results.push({
-          //     id: row.attr("id"),
-          //     sName: ch(children).text(),
-          //     fName: ch(children).attr("sorttable_customkey"),
-          //     profile: ch(children)
-          //       .children("a")
-          //       .attr("href")
-          //   });
-          embed.addField(
-            $(children).attr("sorttable_customkey"),
-            `ID: ${row.attr("id")}`,
-            true
-          );
-        }
-      });
-      //   message.reply(
-      //     `Umu found
-      //      \`${results.map((val, i) => `${val.fName} - ${val.id}`)}\``
-      //   );
-      if (embed.fields.length > 0) message.channel.send({ embed });
-      else message.channel.send("Umu couldn't find anything 😭");
-    });
+  
 };
 
 exports.conf = {
